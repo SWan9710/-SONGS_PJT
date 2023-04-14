d0639af253f57aa17d1e7e074ee241aa5ebb4a9a
krkdhs6240 <wjdtndhks1212@gmail.com>
2023-04-14 17:18:48 +0900

    create accounts

--- song/accounts/models.py
+++ song/accounts/models.py
@@ -1,3 +1,7 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
+class User(AbstractUser):
+    # follow 모델 설정
+    followings = models.ManyToManyField('self', symmetrical=False, related_name='followers')
--- song/accounts/views.py
+++ song/accounts/views.py
@@ -1,3 +1,94 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import AuthenticationForm, PasswordChangeForm
+from django.contrib.auth import login as auth_login
+from django.contrib.auth import logout as auth_logout
+from django.contrib.auth import update_session_auth_hash
+from django.contrib.auth import get_user_model
+from django.views.decorators.http import require_http_methods
+from .forms import CustomUserCreationForm, CustomUserChangeForm
 
 # Create your views here.
+@require_http_methods(['POST', 'GET'])
+def login(request):
+    if request.user.is_authenticated:
+        return redirect('musics:index')
+    if request.method == 'POST':
+        form = AuthenticationForm(request, request.POST)
+        if form.is_valid():
+            auth_login(request, form.get_user())
+            return redirect('musics:index')
+    else:
+        form = AuthenticationForm()
+    context = {'form' : form}
+    return render(request, 'accounts/login.html', context)
+
+@require_http_methods(['POST'])
+def logout(request):
+    auth_logout(request)
+    return redirect('musics:index')
+
+@require_http_methods(['POST', 'GET'])
+def signup(request):
+    if request.method =='POST':
+        form = CustomUserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            auth_login(request, user)
+            return redirect('musics:index')
+    else:
+        form = CustomUserCreationForm()
+    context = {'form' : form}
+    return render(request, 'accounts/signup.html', context)
+
+@require_http_methods(['POST'])
+def delete(request):
+    request.user.delete()
+    return redirect('musics:index')
+
+@require_http_methods(['POST', 'GET'])
+def update(request):
+    if request.method == "POST":
+        form = CustomUserChangeForm(request.POST, instance=request.user)
+        if form.is_valid():
+            form.save()
+            return redirect('musics:index')
+    else:
+        form = CustomUserChangeForm(instance = request.user)
+    context={'form':form}
+    return render(request, 'accounts/update.html', context)
+
+@require_http_methods(['POST', 'GET'])
+def change_password(request):
+    if request.method == 'POST':
+        form = PasswordChangeForm(request.user, request.POST)
+        if form.is_valid():
+            form.save()
+            update_session_auth_hash(request, form.user)
+            return redirect('musics:index')
+    else:
+        form = PasswordChangeForm(request.user)
+    context = {'form' : form}
+    return render (request, 'accounts/change_password.html', context)
+
+
+# profile view 함수 구현
+@require_http_methods(['GET'])
+def profile(request, username):
+    person = get_user_model().objects.get(username=username)
+    context = {
+        'person' : person,
+    }
+    return render(request, 'accounts/profile.html', context)
+
+# follow view 함수 구현
+@require_http_methods(['POST'])
+def follow(request, user_pk):
+    if request.user.is_authenticated:
+        person = get_user_model().objects.get(pk=user_pk)
+        if person != request.user:
+            if person.followers.filter(pk=request.user.pk).exists():
+                person.followers.remove(request.user)
+            else:
+                person.followers.add(request.user)
+        return redirect('accounts:profile', person.username)
+    return redirect('accounts:login')
